758eaf962b4c60560c3796bbf8a728ee1c0c3a1c
Aleksey Soloviov <lelkaklel@gmail.com>
2021-02-14 18:47:17 +0300

    pull_and_deploy.sh script changed

--- pull_and_deploy.sh
+++ pull_and_deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 git pull
+docker build -t lelkaklel/docker-airflow:latest --no-cache .
+docker push lelkaklel/docker-airflow:latest
 docker stack deploy -c docker-compose.yml data_works_cloud
